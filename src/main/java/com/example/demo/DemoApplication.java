f12a8319af4a635727048d5774385ffcea3106d8
massil <massilabba06@gmail.com>
2025-04-01 16:45:09 +0100

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,22 @@
-package com.example.demo;
-
+package com.example.dockerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DockerDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+}
 
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
 }
